799607a564bf32c2f1a9e77c67f90ff8301b6368
akemalyildirim1 <akemalyildirim1@gmail.com>
2023-09-02 02:22:16 +0300

    refactor(main.py): use migration file in the main file
    #5

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 """Main file."""
 import asyncio
+from pathlib import Path
 from src.service.start import Start
+from src.service.migration_files import MigrationFiles
+
 
 start = Start()
+migration_files = MigrationFiles()
 
 
-async def main(path):
+async def main():
     """Main function."""
-    await start(path=path)
+    # await start(path=path)
+    await migration_files(folder_path=Path("temp"), name="new_migration")
 
 
-asyncio.run(main("pydbmigrations.yaml"))
+asyncio.run(main())
