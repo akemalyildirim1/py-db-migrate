a34aacf51c91c5962bb3d229716f97169b314e44
akemalyildirim1 <akemalyildirim1@gmail.com>
2023-09-02 02:19:41 +0300

    test(conftest.py): add fixture for adding and removing temp file for testing
    #5

--- tests/unit/service/test_start.py
+++ tests/unit/service/test_start.py
@@ -3,8 +3,8 @@ import aiofiles.os
 import pytest
 import yaml
 from pathlib import Path
-from shutil import rmtree
 
+from tests.conftest import use_temp_file  # noqa: F401
 from src.service.start import Start
 
 
@@ -14,51 +14,37 @@ def start_service() -> Start:
 
 
 class TestStart:
-    async def test_start_new_config(self, start_service):
+    async def test_start_new_config(self, start_service, use_temp_file):
         """
         Case: Config file doesn't exist. Create a new one.
         """
-        try:
-            await aiofiles.os.mkdir("temp")
-            await start_service(Path("temp/config.yaml"))
+        await start_service(Path("temp/config.yaml"))
 
-            with open("./temp/config.yaml", mode="r") as file:
-                content = yaml.load(file, Loader=yaml.FullLoader)
+        with open("./temp/config.yaml", mode="r") as file:
+            content = yaml.load(file, Loader=yaml.FullLoader)
 
-            assert all([key in content for key in ("database", "migration_directory")])
-        finally:
-            rmtree("./temp")
+        assert all([key in content for key in ("database", "migration_directory")])
 
-    async def test_start_existing(self, start_service):
+    async def test_start_existing(self, start_service, use_temp_file):
         """
         Case: Config file exists. Do not do anything.
         """
-        try:
-            await aiofiles.os.mkdir("temp")
-            async with aiofiles.open("temp/config.yaml", mode="w") as file:
-                await file.write("test")
-            await start_service(Path("temp/config.yaml"))
+        async with aiofiles.open("temp/config.yaml", mode="w") as file:
+            await file.write("test")
+        await start_service(Path("temp/config.yaml"))
 
-            async with aiofiles.open("temp/config.yaml", mode="r") as file:
-                content = await file.read()
+        async with aiofiles.open("temp/config.yaml", mode="r") as file:
+            content = await file.read()
 
-            assert content == "test"
-        finally:
-            rmtree("./temp")
+        assert content == "test"
 
 
 class TestCheckExistenceOfFile:
-    async def test_check_existence_of_file_true(self, start_service):
-        try:
-            await aiofiles.os.mkdir("./temp")
-            async with aiofiles.open("temp/config.yaml", mode="w") as file:
-                await file.write("test")
-            result = await start_service.check_existence_of_file(
-                Path("temp/config.yaml")
-            )
-            assert result is True
-        finally:
-            rmtree("./temp")
+    async def test_check_existence_of_file_true(self, start_service, use_temp_file):
+        async with aiofiles.open("temp/config.yaml", mode="w") as file:
+            await file.write("test")
+        result = await start_service.check_existence_of_file(Path("temp/config.yaml"))
+        assert result is True
 
     async def test_check_existence_of_file_false(self, start_service):
         result = await start_service.check_existence_of_file(Path("temp/config.yaml"))
@@ -66,22 +52,17 @@ class TestCheckExistenceOfFile:
 
 
 class TestCreateConfigurationFile:
-    async def test_create_configuration_file(self, start_service):
-        try:
-            await aiofiles.os.mkdir("./temp")
-            start_service.create_configuration_file(Path("./temp/config.yaml"))
+    async def test_create_configuration_file(self, start_service, use_temp_file):
+        start_service.create_configuration_file(Path("./temp/config.yaml"))
 
-            with open("./temp/config.yaml", mode="r") as file:
-                content = yaml.load(file, Loader=yaml.FullLoader)
+        with open("./temp/config.yaml", mode="r") as file:
+            content = yaml.load(file, Loader=yaml.FullLoader)
 
-            assert content["database"] == {
-                "host": "localhost",
-                "port": 5432,
-                "user": "your_db_username",
-                "password": "your_db_password",
-                "name": "your_db_name",
-            }
-            assert content["migration_directory"] == "pydbmigrations"
-
-        finally:
-            rmtree("./temp")
+        assert content["database"] == {
+            "host": "localhost",
+            "port": 5432,
+            "user": "your_db_username",
+            "password": "your_db_password",
+            "name": "your_db_name",
+        }
+        assert content["migration_directory"] == "pydbmigrations"
