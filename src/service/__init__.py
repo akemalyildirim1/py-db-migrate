49c3381116628b464a026731e3682516735a17d4
akemalyildirim1 <akemalyildirim1@gmail.com>
2023-09-02 01:18:27 +0300

    feat(service/start): add start service
    `Start `service is added. This service will be used for creating an inital configuration file.
    
    #4

--- src/service/__init__.py
+++ src/service/__init__.py
@@ -1,1 +1,9 @@
 """Service module."""
+from abc import ABC
+from pydantic import BaseModel, ConfigDict
+
+
+class ServiceABC(ABC, BaseModel):
+    """ServiceABC class."""
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
