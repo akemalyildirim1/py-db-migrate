99da0085dac91758736e147e782d0ea78e3e6789
akemalyildirim1 <akemalyildirim1@gmail.com>
2023-09-02 01:19:01 +0300

    feat(main.py): add start service to main for testing
    #4

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
 """Main file."""
+import asyncio
+from src.service.start import Start
+
+start = Start()
+
+
+async def main(path):
+    """Main function."""
+    await start(path=path)
+
+
+asyncio.run(main("pydbmigrations.yaml"))
