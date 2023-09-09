4365d565fff243e48aedf368778d045b65bf8594
akemalyildirim1 <akemalyildirim1@gmail.com>
2023-09-10 00:58:07 +0300

    feat(main.py): add up command to app
    #10

--- py_db_migrate/main.py
+++ py_db_migrate/main.py
@@ -6,15 +6,21 @@ from pathlib import Path
 from typing_extensions import Annotated
 
 from py_db_migrate.configuration import Configuration, get_configuration
+from py_db_migrate.database.postgresql import PSql
+from py_db_migrate.logger import get_logger
 from py_db_migrate.service.start import Start
 from py_db_migrate.service.migration_files import MigrationFiles
+from py_db_migrate.service.migration import Migration
 
 app = typer.Typer(help="Awesome CLI user manager.")
 
 CONFIGURATION_FILE_PATH = Path("./py-db-migration.yaml")
 
+logger = get_logger()
+
 
 @app.command("init")
+@app.command("start")
 def start():
     """Create an initial configuration file.
 
@@ -34,52 +40,22 @@ def create(name: Annotated[str, typer.Argument(help="Name of the SQL files.")]):
     )
 
 
-#
-# @app.command()
-# def delete(
-#     username: str,
-#     force: bool = typer.Option(
-#         ...,
-#         prompt="Are you sure you want to delete the user?",
-#         help="Force deletion without confirmation.",
-#     ),
-# ):
-#     """
-#     Delete a user with USERNAME.
-#
-#     If --force is not used, will ask for confirmation.
-#     """
-#     if force:
-#         print(f"Deleting user: {username}")
-#     else:
-#         print("Operation cancelled")
-#
-#
-# @app.command()
-# def delete_all(
-#     force: bool = typer.Option(
-#         ...,
-#         prompt="Are you sure you want to delete ALL users?",
-#         help="Force deletion without confirmation.",
-#     )
-# ):
-#     """
-#     Delete ALL users in the database.
-#
-#     If --force is not used, will ask for confirmation.
-#     """
-#     if force:
-#         print("Deleting all users")
-#     else:
-#         print("Operation cancelled")
-#
-#
-# @app.command()
-# def init():
-#     """
-#     Initialize the users database.
-#     """
-#     print("Initializing user database")
+@app.command("up")
+def migration_up():
+    """Run the new migration files."""
+    configuration: Configuration = get_configuration(path=CONFIGURATION_FILE_PATH)
+    psql: PSql = PSql(**(configuration.database.model_dump()))
+
+    migration: Migration = Migration(database=psql)
+    try:
+        asyncio.run(
+            migration(
+                migration_folder=Path(configuration.migration_directory),
+                migration_table="pydbmigration",
+            )
+        )
+    except Exception as e:
+        logger.critical(str(e))
 
 
 if __name__ == "__main__":
